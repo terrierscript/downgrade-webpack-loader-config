73dd8940994773ce2dc7e3863d3daf909a5322f4
inuscript <inuscript@gmail.com>
2016-12-08 21:45:13 +0900

    initial commit

---
+++ degrate.js
@@ -0,0 +1,27 @@
+// https://webpack.js.org/guides/migrating/
+const flatten = require("lodash.flatten")
+
+const getLoader = (loaderConf) => {
+  const loader = (typeof loaderConf === "string") ? loaderConf :loaderConf.loader
+  return { loader }
+}
+const getQuery = (loader) => (
+  (!!loader.options) ? { query: loader.options } : {}
+)
+
+
+const convert = (conf) => {
+  return flatten(conf.use.map( (loaderConf) => {
+    return Object.assign(
+      { test: conf.test },
+      getLoader(loaderConf),
+      getQuery(loaderConf)
+    )
+  }))
+}
+
+module.exports = (config) => {
+  return flatten(config.map( (conf) => {
+    return convert(conf)
+  }))
+}
---
+++ test/index.js
@@ -0,0 +1,86 @@
+const assert = require('assert')
+const degrate = require('../degrate')
+const migrate = require('../migrate')
+const util = require('util')
+describe('', () => {
+  const confV1 = [
+    {
+      test: /.js?$/,
+      loaders: ['babel-loader', 'eslint-loader'],
+      exclude: /node_modules/
+    },
+    {
+      test: /\.css$/,
+      loader: 'style-loader'
+    },
+    {
+      test: /\.css$/,
+      loader: 'css-loader',
+      query: {
+        modules: true,
+        localIdentName: '[name]_[local]_[hash:base64:5]'
+      }
+    },
+    {
+      test: /\.css$/,
+      loader: 'postcss-loader'
+    },
+    { test: /\.(png|jpeg|svg)$/, loader: "url-loader" },
+  ]
+
+  const confV2 = [
+    {
+      test: /.js?$/,
+      use: ['babel-loader', 'eslint-loader'],
+      exclude: /node_modules/
+    },
+    {
+      // css読み込み。create-react-appを参考にした
+      test: /\.css$/,
+      use: [
+        { loader: 'style-loader' },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            localIdentName: '[name]_[local]_[hash:base64:5]'
+          }
+        },
+        { loader: 'postcss-loader' }
+      ]
+    },
+    {
+      test: /\.(png|jpeg|svg)$/,
+      use: [
+        "url-loader"
+      ]
+    }
+  ]
+  // it('1 -> 2', () => {
+  //   const output = migrate(confV1)
+  //   assert.deepEqual(output, confV2)
+  // })
+  it('2 -> 1', () => {
+    const output = degrate(confV2)
+    assert.deepEqual(output, [
+      { test: /.js?$/, loader: 'babel-loader' },
+      { test: /.js?$/, loader: 'eslint-loader' },
+      { test: /\.css$/, loader: 'style-loader' },
+      { test: /\.css$/,
+        loader: 'css-loader',
+        query:
+         { modules: true,
+           localIdentName: '[name]_[local]_[hash:base64:5]' } },
+      { test: /\.css$/, loader: 'postcss-loader' },
+      { test: /\.(png|jpeg|svg)$/, loader: 'url-loader' }
+    ])
+  })
+  it('pre, post', () => {
+    {
+      test: /.js?$/,
+      enforce: "pre"
+      use: ['eslint-loader'],
+    },
+
+  })
+})
